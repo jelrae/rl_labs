d851784e1c755b47878710014a0f5ed9afc3677b
jordan <jordanalexearle@gmail.com>
2020-09-05 18:13:01 +0200

    Might be fixed

--- 1_lab/dp_autograde.py
+++ 1_lab/dp_autograde.py
@@ -1,2 +1,26 @@
 import numpy as np
 from collections import defaultdict
+
+def policy_iter_v(env, policy_eval_v=policy_eval_v, discount_factor=1.0):
+    """
+    Policy Iteration Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
+    
+    Args:
+        env: The OpenAI envrionment.
+        policy_eval_v: Policy Evaluation function that takes 3 arguments:
+            policy, env, discount_factor.
+        discount_factor: gamma discount factor.
+        
+    Returns:
+        A tuple (policy, V). 
+        policy is the optimal policy, a matrix of shape [S, A] where each state s
+        contains a valid probability distribution over actions.
+        V is the value function for the optimal policy.
+        
+    """
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+    # YOUR CODE HERE
+    raise NotImplementedError
+    return policy, V
