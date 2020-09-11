895596f18953d91836abc0f9ecb0605081372a05
melle_vessies <melle.vessies@gmail.com>
2020-09-11 12:14:51 +0200

    bugfixes, only final MC part left

--- 2_lab/mc_autograde.py
+++ 2_lab/mc_autograde.py
@@ -22,8 +22,9 @@ class SimpleBlackjackPolicy(object):
             Numpy array filled with probabilities (same length as states and actions)
         """
         probs = []
-        for state in states:
-            probs.append([self.sample_action(state), 1 - self.sample_action(state)])
+        for state, action in zip(states, actions):
+            policyaction = self.sample_action(state)
+            probs.append(float(policyaction == action))
         
         return np.array(probs)
     
@@ -61,15 +62,17 @@ def sample_episode(env, policy):
     
     states.append(env.reset())
     t = 0
-    done = False
-    while not done:
+    while True:
         a = policy.sample_action(states[t])
         sp, r, done, _ = env.step(a)
         
-        states.append(sp)
         actions.append(a)
         rewards.append(r)
         dones.append(done)
+        if done:
+            break
+        
+        states.append(sp)
         t += 1
     
     return states, actions, rewards, dones
@@ -94,327 +97,17 @@ def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_funct
     # Keeps track of current V and count of returns for each state
     # to calculate an update.
     V = defaultdict(float)
+    
     returns = defaultdict(list)
+    
     returns_count = defaultdict(float)
-    for i in range(5):
+    
+    for i in tqdm(range(num_episodes)):
         S = []
         states, actions, rewards, dones = sampling_function(env, policy)
-        G, R = 0 
-        for idx in range(0, len(states) - 1, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx][0]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    for i in range(5):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
-        G; R = 0
-        for idx in range(0, len(states) - 1, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx][0]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    for i in range(5):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
+        
         G = 0
-        R = 0
-        for idx in range(0, len(states) - 1, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx][0]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    for i in range(5):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
-        G = 0
-        R = 0
-        for idx in range(len(states) - 1, 0, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx][0]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    for i in range(5):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
-        G = 0
-        R = 0
-        for idx in range(len(states) - 2, 0, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx][0]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    for i in range(1000):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
-        G = 0
-        R = 0
-        for idx in range(len(states) - 2, 0, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx][0]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    for i in range(num_episodes):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
-        G = 0
-        R = 0
-        for idx in range(len(states) - 2, 0, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx][0]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    
-    for i in range(num_episodes):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
-        G = 0
-        R = 0
-        for idx in range(len(states) - 2, 0, -1):
-            R += rewards[idx]
-            G = discount_factor * G + R
-            
-            s = states[idx]
-            if s not in S:
-                S.append(s)
-                returns[s].append(G)
-                V[s] = sum(returns[s])/len(returns[s])
-                
-    return V
-
-def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_function=sample_episode):
-    """
-    Monte Carlo prediction algorithm. Calculates the value function
-    for a given policy using sampling.
-    
-    Args:
-        env: OpenAI gym environment.
-        policy: A policy which allows us to sample actions with its sample_action method.
-        num_episodes: Number of episodes to sample.
-        discount_factor: Gamma discount factor.
-        sampling_function: Function that generates data from one episode.
-    
-    Returns:
-        A dictionary that maps from state -> value.
-        The state is a tuple and the value is a float.
-    """
-
-    # Keeps track of current V and count of returns for each state
-    # to calculate an update.
-    V = defaultdict(float)
-    
-    returns = defaultdict(list)
-    returns_count = defaultdict(float)
-    
-    for i in range(num_episodes):
-        S = []
-        states, actions, rewards, dones = sampling_function(env, policy)
-        G = 0
-        for idx in range(len(states) - 2, 0, -1):
+        for idx in range(len(states) - 1, -1, -1):
             G = discount_factor * G + rewards[idx]
             
             s = states[idx]
@@ -422,5 +115,89 @@ def mc_prediction(env, policy, num_episodes, discount_factor=1.0, sampling_funct
                 S.append(s)
                 returns[s].append(G)
                 V[s] = sum(returns[s])/len(returns[s])
+                
+    return V
+
+class RandomBlackjackPolicy(object):
+    """
+    A random BlackJack policy.
+    """
+    def get_probs(self, states, actions):
+        """
+        This method takes a list of states and a list of actions and returns a numpy array that contains 
+        a probability of perfoming action in given state for every corresponding state action pair. 
+
+        Args:
+            states: a list of states.
+            actions: a list of actions.
+
+        Returns:
+            Numpy array filled with probabilities (same length as states and actions)
+        """
+        
+        return np.zeros(len(states)) + 0.5
+    
+    def sample_action(self, state):
+        """
+        This method takes a state as input and returns an action sampled from this policy.  
+
+        Args:
+            state: current state
+
+        Returns:
+            An action (int).
+        """
+        return np.random.choice(a=[0, 1])
+
+def mc_importance_sampling(env, behavior_policy, target_policy, num_episodes, discount_factor=1.0,
+                           sampling_function=sample_episode):
+    """
+    Monte Carlo prediction algorithm. Calculates the value function
+    for a given target policy using behavior policy and ordinary importance sampling.
+    
+    Args:
+        env: OpenAI gym environment.
+        behavior_policy: A policy used to collect the data.
+        target_policy: A policy which value function we want to estimate.
+        num_episodes: Number of episodes to sample.
+        discount_factor: Gamma discount factor.
+        sampling_function: Function that generates data from one episode.
+    
+    Returns:
+        A dictionary that maps from state -> value.
+        The state is a tuple and the value is a float.
+    """
+
+        # Keeps track of current V and count of returns for each state
+    # to calculate an update.
+
+    # nvm ignore this
+    
+    #     V = defaultdict(float)
+#     C = defaultdict(float)
+    
+    
+#     returns = defaultdict(list)
+    
+    
+#     returns_count = defaultdict(float)
+    
+#     for i in range(num_episodes):
+#         S = []
+#         states, actions, rewards, dones = sampling_function(env, behavior_policy)
+#         G = 0
+#         W = 1
+        
+#         for idx in range(len(states) - 1, 0, -1):
+#             G = discount_factor * G + rewards[idx]            
+#             s = states[idx]
+#             a = actions[idx]
+            
+#             C[s] = C[s] + W
+            
+#             if s not in S:
+#                 S.append(s)
+#                 returns[s].append(G)
+#                 V[s] = sum(returns[s])/len(returns[s])
                 
     return V
